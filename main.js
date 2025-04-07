86616e95224e042de8da6948a9190d3a256d68d2
Sahil Suman <sahilsuman1202@gmail.com>
2025-04-07 23:13:07 +0530

    basic files

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 // node js revision from scratch
+console.log("Hello World!")
+
+// npm => node package manager
+// npm init -y => create package.json file
+// npm install express => install express package
+// npm install express --save => save express package to package.json file
+
+const math = require('./math');
+// Modules in nodejs : 
+
+
+console.log(math);
